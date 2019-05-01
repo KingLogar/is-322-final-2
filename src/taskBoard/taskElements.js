763afe6f38e0e5552d144597301b463468a72154
Logan <lh252@njit.edu>
2019-05-01 19:34:28 -0400

    Cleaning out old code

--- src/taskBoard/taskElements.js
+++ src/taskBoard/taskElements.js
@@ -4,40 +4,8 @@ class TodoElement extends Component{
     render() {
         return (<div className='taskBoard-TodoElement'>
             <div className='taskBoard-element-title'>{ this.props.title }</div>
-            <div className='taskBoard-element-type'>Type: { this.props.type }</div>
-            <button type="button" onClick={ () => { this.props.startOnclick(this.props.id)} } className='taskBoard-element-button btn btn-outline-primary btn-sm' >Start</button>
-        </div>)
-    }
-}
-
-class InProgressElement extends Component {
-    render() {
-        return (<div className='taskBoard-TodoElement'>
-            <div className='taskBoard-element-title'>{ this.props.title }</div>
-            <div className='taskBoard-element-type'>Type: { this.props.type }</div>
-            <button  type="button" onClick={ () => {this.props.sendBackOnclick(this.props.id)} } className='taskBoard-element-button btn btn-outline-primary btn-sm' >Send Back</button>
-            <button type="button" onClick={ () => {this.props.reviewOnclick(this.props.id)} } className='taskBoard-element-button btn btn-outline-secondary btn-sm' >Review</button>
-        </div>)
-    }
-}
-
-class ReviewElement extends Component {
-    render() {
-        return (<div className='taskBoard-TodoElement'>
-            <div className='taskBoard-element-title'>{ this.props.title }</div>
-            <div className='taskBoard-element-type'>Type: { this.props.type }</div>
-            <button type="button" onClick={ () => {this.props.backToWorkOnclick(this.props.id)} } className='taskBoard-element-button btn btn-outline-primary btn-sm' >Back to Work</button>
-            <button type="button" onClick={ () => {this.props.doneOnclick(this.props.id)} } className='taskBoard-element-button btn btn-outline-secondary btn-sm' >Done</button>
-        </div>)
-    }
-}
-
-class DoneElement extends Component {
-    render() {
-        return (<div className='taskBoard-TodoElement'>
-            <div className='taskBoard-element-title'>{ this.props.title }</div>
-            <div className='taskBoard-element-type'>Type: { this.props.type }</div>
-            <button type="button" onClick={ () => {this.props.reviewAgainOnclick(this.props.id)} } className='taskBoard-element-button btn btn-outline-primary btn-sm' >Review again</button>
+            <div className='taskBoard-element-type'>Balance: { /*show balance*/ }</div>
+            <button type="button" onClick={ () => { /*view details*/ }}>View</button>
         </div>)
     }
 }
